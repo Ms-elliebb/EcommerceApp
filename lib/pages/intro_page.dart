3cf0b40196dae0d3b368b76a4067aebea117c203
ellie <74204751+Ms-elliebb@users.noreply.github.com>
2024-04-29 01:43:37 +0300

    intro page

---
+++ lib/pages/intro_page.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class IntroPage extends StatelessWidget {
+  const IntroPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 224, 224, 224),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal:25.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              //logo
+              Padding(
+                padding: const EdgeInsets.all(30.0),
+                child: Image.asset('lib/images/Puma_Logo.png',
+                height: 180,
+                ),
+              ), 
+          
+              const SizedBox(height:48),
+          
+          
+              //title
+              const Text(
+                'Faster. When youre fast, you set the pace.',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize:25,
+          
+                ),
+                ),
+                const SizedBox(height: 20),
+          
+              //sub title
+              const Text(
+                'Brand new sneakers.',
+                style: TextStyle(
+                  
+                  fontSize:18,
+                  color: Colors.black38,
+          
+                ),
+                textAlign: TextAlign.center,
+                ),
+                const SizedBox(height: 48),
+          
+              //start now button
+              GestureDetector(
+               // onTap: () => Navigator.push(context,
+                //MaterialPageRoute(
+                  //builder: (context) => HomePage(),
+                  //)
+                  //),
+                child: Container(
+                  decoration: BoxDecoration(color:Colors.pink[700],
+                  borderRadius: BorderRadius.circular(40)
+                  ),
+                  padding:const EdgeInsets.all(25),
+                  child: const  Center(
+                    child: const Text(
+                      'Shop Now',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18,
+                      ),),
+                  ),
+                  ),
+              )
+          
+          ],),
+        ),
+      )
+    );
+  }
+}
